a296011c178bcc3e438250640fcfde7dd3b8fc74
Fabian Zimmermann <fabian.zimmermann@noris.de>
2017-01-20 14:13:36 +0100

    Manage service only if manage_service is true

--- manifests/api.pp
+++ manifests/api.pp
@@ -77,35 +77,35 @@ class aodh::api (
     } else {
       $service_ensure = 'stopped'
     }
-  }
 
-  if $sync_db {
-    include ::aodh::db::sync
-  }
-
-  if $service_name == $::aodh::params::api_service_name {
-    service { 'aodh-api':
-      ensure     => $service_ensure,
-      name       => $::aodh::params::api_service_name,
-      enable     => $enabled,
-      hasstatus  => true,
-      hasrestart => true,
-      tag        => 'aodh-service',
-    }
-  } elsif $service_name == 'httpd' {
-    include ::apache::params
-    service { 'aodh-api':
-      ensure => 'stopped',
-      name   => $::aodh::params::api_service_name,
-      enable => false,
-      tag    => 'aodh-service',
+    if $sync_db {
+      include ::aodh::db::sync
     }
 
-    # we need to make sure aodh-api/eventlet is stopped before trying to start apache
-    Service['aodh-api'] -> Service[$service_name]
-  } else {
-    fail("Invalid service_name. Either aodh/openstack-aodh-api for running \
-as a standalone service, or httpd for being run by a httpd server")
+    if $service_name == $::aodh::params::api_service_name {
+      service { 'aodh-api':
+        ensure     => $service_ensure,
+        name       => $::aodh::params::api_service_name,
+        enable     => $enabled,
+        hasstatus  => true,
+        hasrestart => true,
+        tag        => 'aodh-service',
+      }
+    } elsif $service_name == 'httpd' {
+      include ::apache::params
+      service { 'aodh-api':
+        ensure => 'stopped',
+        name   => $::aodh::params::api_service_name,
+        enable => false,
+        tag    => 'aodh-service',
+      }
+
+      # we need to make sure aodh-api/eventlet is stopped before trying to start apache
+      Service['aodh-api'] -> Service[$service_name]
+    } else {
+      fail("Invalid service_name. Either aodh/openstack-aodh-api for running \
+  as a standalone service, or httpd for being run by a httpd server")
+    }
   }
 
   aodh_config {
